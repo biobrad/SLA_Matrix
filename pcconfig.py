8761a8da3868d1e873b459bcf4a2d7d5e4526c28
Brad Hart <bradhartmail@gmail.com>
2023-03-21 15:09:36 +1030

    Update pcconfig.py

--- pcconfig.py
+++ pcconfig.py
@@ -1,7 +1,8 @@
 import pynecone as pc
 
 config = pc.Config(
-    app_name="newmatrix",
+    app_name="slamatrix",
+    api_url="192.168.10.100:8000",
+    bun_path="$HOME/.bun/bin/bun",
     db_url="sqlite:///pynecone.db",
-    env=pc.Env.DEV,
 )
