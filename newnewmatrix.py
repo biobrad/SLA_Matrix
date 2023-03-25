1465a505054984097948973f26e01d506eb31c58
Brad Hart <bradhartmail@gmail.com>
2023-03-25 20:18:44 +1030

    Update newnewmatrix.py

--- newnewmatrix.py
+++ newnewmatrix.py
@@ -11,10 +11,10 @@ class App(customtkinter.CTk):
         self.title("SLA Matrix")
         self.minsize(300, 200)
            
-        self.grid_rowconfigure(0, weight=1)
-        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(0, weight=0)
+        self.grid_columnconfigure(0, weight=0)
         self.mainframe = customtkinter.CTkFrame(self, width=300, height=500)
-        self.mainframe.grid(row=0, column=0, padx=20, pady=20)
+        self.mainframe.grid(row=0, column=0,columnspan=2, padx=20, pady=20)
         self.mainframe.grid_rowconfigure((0,1,2), weight=1)
         self.mainframe.grid_columnconfigure(0, weight=1)
         self.logo_label = customtkinter.CTkLabel(self.mainframe, text="SLA MATRIX", fg_color=("white", "#3399FF"), corner_radius=8, font=customtkinter.CTkFont(size=30, weight="bold"))
@@ -26,7 +26,43 @@ class App(customtkinter.CTk):
         self.datentime = ""
         self.starttime = ""
         self.endtime = ""
-        
+
+        ### Start Time validate ###
+        def show_message2(error='', color='transparent'):
+            self.label_error2.configure(text = error, fg_color=color)
+    
+        def validate2(value):
+            try:
+                if value == '' or len(value) == 4 and datetime.strptime(value, '%H%M'):
+                    show_message2()
+                    return True
+                else:
+                    show_message2('INVALID fmt=HHMM', 'red')
+                    return False
+            except ValueError:
+                show_message2('INVALID fmt=HHMM', 'red')
+                return False
+
+        self.vcmd2 = (self.register(validate2), '%P')
+        ###
+        ### end Time validate ###
+        def show_message3(error='', color='transparent'):
+            self.label_error3.configure(text = error, fg_color=color)
+    
+        def validate3(value):
+            try:
+                if value == '' or len(value) == 4 and datetime.strptime(value, '%H%M'):
+                    show_message3()
+                    return True
+                else:
+                    show_message3('INVALID fmt=HHMM', 'red')
+                    return False
+            except ValueError:
+                show_message3('INVALID fmt=HHMM', 'red')
+                return False
+
+        self.vcmd3 = (self.register(validate3), '%P')
+        ######
         
         def combobox_callback(choice):
             self.sla_var = choice
@@ -34,7 +70,7 @@ class App(customtkinter.CTk):
         def button_callback(datentime, sla_var, starttime, endtime):
             print("create: " + datentime, "SLA: " + sla_var, "Start: " + starttime, "End: " + endtime)
                                                                               
-        self.grid_rowconfigure((0,1,2,3,4,5,6,7), weight=1)
+        self.grid_rowconfigure((0,1,2,3,4,5,6,7,8), weight=1)
         self.grid_columnconfigure((0,1), weight=1)
         self.label1 = customtkinter.CTkLabel(master=self.my_frame, text="Copy/Paste SC create date/time", fg_color=("white", "#3399FF"), corner_radius=8, font=customtkinter.CTkFont(size=15, weight="bold"))
         self.label1.grid(row=0, column=0, columnspan=2, padx=20, pady=(20, 0))
@@ -46,50 +82,31 @@ class App(customtkinter.CTk):
         self.label3.grid(row=3, column=0, padx=20, pady=0)
         self.label4 = customtkinter.CTkLabel(master=self.my_frame, text="End", justify="center")
         self.label4.grid(row=3, column=1, padx=20, pady=0)
-        #self.entry2 = customtkinter.CTkEntry(master=self.my_frame, placeholder_text="0700")
-        #self.entry2.configure(validate="key", validatecommand=vcmd, invalidcommand=ivcmd)
-        #self.entry2.grid(row=4, column=0, padx=20)
-        self.create_widgets()
-        self.entry3 = customtkinter.CTkEntry(master=self.my_frame, placeholder_text="2330")
+        self.entry2 = customtkinter.CTkEntry(master=self.my_frame, validate="focusout", validatecommand=self.vcmd2, placeholder_text="HHMM")
+        self.entry2.grid(row=4, column=0, padx=20)
+        
+        self.label_error2 = customtkinter.CTkLabel(self.my_frame, text="", fg_color='transparent')
+        self.label_error2.grid(row=5, column=0, padx=1)
+        
+        self.entry3 = customtkinter.CTkEntry(master=self.my_frame, validate="focusout", validatecommand=self.vcmd3, placeholder_text="HHMM")
         self.entry3.grid(row=4, column=1, padx=20)
+        
+        self.label_error3 = customtkinter.CTkLabel(self.my_frame, text="", fg_color='transparent')
+        self.label_error3.grid(row=5, column=1, padx=1)
+        
         self.label5 = customtkinter.CTkLabel(master=self.my_frame, text="Select SLA in Hours", fg_color=("white", "#3399FF"), corner_radius=8, font=customtkinter.CTkFont(size=15, weight="bold"))
-        self.label5.grid(row=5, column=0, columnspan=2, padx=20, pady=(20, 0))
+        self.label5.grid(row=6, column=0, columnspan=2, padx=20, pady=0)
         self.combobox = customtkinter.CTkComboBox(master=self.my_frame,
                                      values=["4", "8", "12", "24"],
                                      command=combobox_callback,
                                      variable=self.sla_var)
-        self.combobox.grid(row=6, columnspan=2, padx=20, pady=10)
+        self.combobox.grid(row=7, columnspan=2, padx=20, pady=10)
         self.button = customtkinter.CTkButton(master=self.my_frame, command=button_callback, text="Get Calculated SLA", width=200, height=60, fg_color=("white", "green"), font=customtkinter.CTkFont(size=15, weight="bold"))
-        self.button.grid(row=7, column=0, columnspan=2, padx=20, pady=(0,10))
+        self.button.grid(row=8, column=0, columnspan=2, padx=20, pady=(0,10))
         self.textbox = customtkinter.CTkTextbox(master=self.mainframe, width=200, height=60)
         self.textbox.grid(row=2, column=0, columnspan=2, padx=20, pady=(0, 20), sticky="ew")
         self.textbox.configure(state="disabled")
 
-    def show_message(self, error='', color='black'):
-        self.label_error['text'] = error
-        self.entry2['foreground'] = color
-    
-    def validate(self, value):
-        try:
-            if len(value) == 4:
-                datetime.strptime(value, '%H%M')
-                self.show_message()
-                return True
-            else:
-                return False
-        except ValueError:
-            return False
-    def on_invalid(self):
-        self.show_message('Enter Valid Time', 'red')
-
-    def create_widgets(self):
-        vcmd = (self.register(self.validate), '%P')
-        ivcmd = (self.register(self.on_invalid),)
-        self.entry2 = ttk.Entry(master=self.my_frame)
-        self.entry2.config(validate="focusout", validatecommand=vcmd, invalidcommand=ivcmd)
-        self.entry2.grid(row=4, column=0, padx=20)
-        self.label_error = ttk.Label(master=self.my_frame, foreground='red')
-        self.label_error.grid(row=4, column=0, sticky="s", padx=5)
    
 
 if __name__ == "__main__":
