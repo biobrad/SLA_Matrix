a16a34f802764f844403de584f7d7a393cbb0400
Brad Hart <bradhartmail@gmail.com>
2023-04-01 14:17:46 +1030

    Update and rename newnewmatrix.py to tkintermatrix.py

--- tkintermatrix.py
+++ tkintermatrix.py
@@ -22,10 +22,28 @@ class App(customtkinter.CTk):
         self.logo_label.grid(row=0, column=0, columnspan=2, pady=(20, 0))                                
 
         ### Field Validations ###
-        ### Start Time validate ###
+        ### Create date validation ###
+        def show_message1(error='', color='transparent'):
+            self.label_error1.configure(text = error, fg_color=color)
+
+        def validate1(value):
+           try:
+                if value == '' or datetime.strptime(value, "%d-%m-%Y %H:%M:%S"):
+                    show_message1()
+                    return True
+                else:
+                    show_message1('INVALID format = dd-mm-yyyy HH:MM:SS', 'red')
+                    return False
+           except ValueError:
+               show_message1('INVALID format = dd-mm-yyyy HH:MM:SS', 'red')
+               return False
+
+        self.vcmd1 = (self.register(validate1), '%P') 
+        
+        ###Start time validation
         def show_message2(error='', color='transparent'):
             self.label_error2.configure(text = error, fg_color=color)
-    
+        
         def validate2(value):
             try:
                 if value == '' or len(value) == 4 and datetime.strptime(value, '%H%M'):
@@ -79,36 +97,38 @@ class App(customtkinter.CTk):
         self.my_frame = customtkinter.CTkFrame(master=self.mainframe)
         self.my_frame.grid(row=1, column=0, columnspan=2, padx=20, pady=(20, 0))                                                             
         self.grid_rowconfigure((0,1,2,3,4,5,6,7,8), weight=1)
-        self.grid_columnconfigure((0,1), weight=1)
+        self.grid_columnconfigure((0,1,2), weight=1)
         self.label1 = customtkinter.CTkLabel(master=self.my_frame, text="Copy/Paste SC create date/time", font=customtkinter.CTkFont(size=15, weight="bold"))
         self.label1.grid(row=0, column=0, columnspan=2, padx=20, pady=(20, 0))
-        self.entry1 = customtkinter.CTkEntry(master=self.my_frame, placeholder_text="1-1-2023 00:00:00", text_color="white")
+        self.entry1 = customtkinter.CTkEntry(master=self.my_frame, validate="focusout", validatecommand=self.vcmd1, placeholder_text="1-1-2023 00:00:00", text_color="white")
         self.entry1.grid(row=1, column=0, columnspan=2, padx=0, pady=(5,0))
+        self.label_error1 = customtkinter.CTkLabel(self.my_frame, text="", fg_color='transparent', height=10)
+        self.label_error1.grid(row=2, column=0, columnspan=2, padx=1)        
         self.label2 = customtkinter.CTkLabel(master=self.my_frame, text="Customer Availability", font=customtkinter.CTkFont(size=15, weight="bold"))
-        self.label2.grid(row=2, column=0, columnspan=2, padx=20, pady=(20, 0), sticky="n")
+        self.label2.grid(row=3, column=0, columnspan=2, padx=20, pady=(20, 0), sticky="n")
         self.label3 = customtkinter.CTkLabel(master=self.my_frame, text="Start", justify="center")
-        self.label3.grid(row=3, column=0, padx=20, pady=0)
+        self.label3.grid(row=4, column=0, padx=20, pady=0)
         self.label4 = customtkinter.CTkLabel(master=self.my_frame, text="End", justify="center")
-        self.label4.grid(row=3, column=1, padx=20, pady=0)
+        self.label4.grid(row=4, column=1, padx=20, pady=0)
         self.entry2 = customtkinter.CTkEntry(master=self.my_frame, validate="focusout", validatecommand=self.vcmd2, placeholder_text="HHMM")
-        self.entry2.grid(row=4, column=0, padx=20)
+        self.entry2.grid(row=5, column=0, padx=20)
         self.label_error2 = customtkinter.CTkLabel(self.my_frame, text="", fg_color='transparent', height=10)
-        self.label_error2.grid(row=5, column=0, padx=1)
+        self.label_error2.grid(row=6, column=0, padx=1)
         self.entry3 = customtkinter.CTkEntry(master=self.my_frame, validate="focusout", validatecommand=self.vcmd3, placeholder_text="HHMM")
-        self.entry3.grid(row=4, column=1, padx=20)
+        self.entry3.grid(row=5, column=1, padx=20)
         self.label_error3 = customtkinter.CTkLabel(self.my_frame, text="", fg_color='transparent', height=10)
-        self.label_error3.grid(row=5, column=1, padx=1)
+        self.label_error3.grid(row=6, column=1, padx=1)
         self.label5 = customtkinter.CTkLabel(master=self.my_frame, text="Select SLA in Hours", font=customtkinter.CTkFont(size=15, weight="bold"))
-        self.label5.grid(row=6, column=0, columnspan=2, padx=20, pady=0)
+        self.label5.grid(row=7, column=0, columnspan=2, padx=20, pady=0)
         self.combobox = customtkinter.CTkComboBox(master=self.my_frame,
                                      values=["4", "8", "12", "24"]
                                      )
-        self.combobox.grid(row=7, columnspan=2, padx=20, pady=10)
+        self.combobox.grid(row=8, columnspan=2, padx=20, pady=10)
         self.button = customtkinter.CTkButton(master=self.my_frame, command=button_callback, text="Get Calculated SLA", fg_color=('#028900'),border_width=2, border_color=('#028900'), hover_color=('#057103'), width=200, height=60, font=customtkinter.CTkFont(size=15, weight="bold"))
-        self.button.grid(row=8, column=0, columnspan=2, padx=20, pady=(0,10))
+        self.button.grid(row=9, column=0, columnspan=2, padx=20, pady=(0,10))
         self.textbox = customtkinter.CTkTextbox(master=self.mainframe, width=200, height=60)
         self.textbox.grid(row=2, column=0, columnspan=2, padx=20, pady=(0, 20), sticky="ew")
-        self.label6 = customtkinter.CTkLabel(master=self, text="SLA Matrix v0.1, Built and mainted by Brad Hart - d403298", font=customtkinter.CTkFont(size=10))
+        self.label6 = customtkinter.CTkLabel(master=self.mainframe, text="SLA Matrix v0.2, Built and mainted by Brad Hart - d403298", font=customtkinter.CTkFont(size=10))
         self.label6.grid(row=3, column=0, columnspan=2, padx=5, pady=(20,20), sticky="n")
         ### GUI end ###
         
