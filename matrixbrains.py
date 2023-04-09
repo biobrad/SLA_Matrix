53d6b91a83c6cf91b3b5c39a0c7030a00a33579b
Brad Hart <bradhartmail@gmail.com>
2023-04-09 11:32:12 +0930

    Update matrixbrains.py

--- matrixbrains.py
+++ matrixbrains.py
@@ -30,22 +30,23 @@ def getsla(CASECREATEDATETIME, SLA, CUSTSTART, CUSTEND):
     
     #The remaining SLA is calculated by subtracting the day1 SLA usage from the total SLA (this can result in a negative number with short SLAs)
     sla_remainder = timedelta(hours=int(SLA)) - day1_sla
-    print(sla_remainder)
     
     # if sla goes into next day, add 1 day to the day counter
     # if remainder of SLA is negative and availability end time is more than 4 hours away, set new_sla as now + 4hours
+    # This part of the script allows for 'NOW' dates and time for creation of new SLA... can be passed to rules.
     ## (this step may need revisiting to allow for after hours (or a flag created)) if remainder of SLA is negative and availability end is less than 4 hours away, add 1 day and make sla 4 hours from next days start date
     
     if sla_remainder > timedelta(hours=int(0)):
         day_counter += 1
     elif sla_remainder <= timedelta(0) and datetime.combine(now_date, end_delta) - now > timedelta(hours=4):
         new_sla = now + timedelta(hours=4)
-        return new_sla
+        return new_sla.strftime('New SLA Date/time: %d-%m-%Y %H:%M:%S')
     elif sla_remainder <= timedelta(0) and datetime.combine(now_date, end_delta) - now < timedelta(hours=4):
         new_sla = datetime.combine(now_date, start_delta) + timedelta(days=1, hours=4)
-        return new_sla
+        return new_sla.strftime('New SLA Date/time: %d-%m-%Y %H:%M:%S')
         
-    #loop to add days as per customer availbility and remainder of slacount
+    #loop to add days as per customer availbility and remainder of sla after adding days.
+    
     def adddays(start, end, remainder, day_counter, slacount):
         hoursavail = end - start
         slacount = remainder
@@ -62,11 +63,25 @@ def getsla(CASECREATEDATETIME, SLA, CUSTSTART, CUSTEND):
         
     #the reason for setting this as 'create.date and start time' is because we want the slacount to start from the customers available time
     #after the number of days have been added. this only comes into effect if the sla gets days added.
-    new_sla = datetime.combine(create.date, start_delta) + timedelta(days=day_counter) + slacount
-    if new_sla > now:
-        kill myself.
+    new_sla = datetime.combine(create.date(), start_delta) + timedelta(days=day_counter) + slacount
     
-    print(type(slacount))
-    print(str(new_sla))
+    # if new sla ends up in the past make it 4 hours from now. Unless 4 hours from now is outside customer available times, then push it out to 4 hours from customer availability tomorrow.
+    if new_sla < now and datetime.combine(now_date, end_delta) - now > timedelta(hours=4):
+        new_sla = now + timedelta(hours=4)
+        return new_sla.strftime('New SLA Date/time: %d-%m-%Y %H:%M:%S')
+    elif new_sla < now and datetime.combine(now_date, end_delta) - now < timedelta(hours=4):
+        new_sla = datetime.combine(now_date, start_delta) + timedelta(days=1, hours=4)
+        return new_sla.strftime('New SLA Date/time: %d-%m-%Y %H:%M:%S')
     
-    # rules and public holidays/weekends... kill me
+    return new_sla.strftime('New SLA Date/time: %d-%m-%Y %H:%M:%S')
+    
+    
+#Rules from old matrix
+#if SLA falls = 7am-10am then make latest available the day before IF greater than 4 hours to cust COB 
+#If > 4 hours to cust COB & SLA falls AH on that day, make SLA cust COB
+#If SLA falls before 10am next day & < 4 hours to COB push to cust start next day (7am or greater) & display to user "advise cust of delayed SLA, negotiate AH Appointment if customer requires."
+#If restoration before customer COB, leave restoration as is
+#If afterhours required recommit to customers close of business.
+#If 24*7 Add after hours tick box rules and public holidays/weekends... kill me
+    
+    
