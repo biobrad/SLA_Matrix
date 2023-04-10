f1b31b2756ea5522c6c3f8da93304022446fee51
Brad Hart <bradhartmail@gmail.com>
2023-04-10 15:40:42 +0930

    Update matrixbrains.py

--- matrixbrains.py
+++ matrixbrains.py
@@ -15,9 +15,9 @@ def getsla(CASECREATEDATETIME, SLA, CUSTSTART, CUSTEND):
     day_counter = 0
     slacount = 0
     new_sla=""
-    weekend_date=""
-    wknd_day=""
-    Delay="\nAdvise cust of delayed SLA, negotiate AH Appointment if customer requires."
+    Delay="-Advise cust of delayed SLA, negotiate AH Appointment if customer requires."
+    output=""
+    
     
     def adddays(start, end, remainder, day_counter, slacount):
         hoursavail = end - start
@@ -42,14 +42,42 @@ def getsla(CASECREATEDATETIME, SLA, CUSTSTART, CUSTEND):
         else:
             return "nope", "nope"
     
-    def returnsla(weekend_date, wknd_day, new_sla, message=""):
+    def in_between(now, start, end):
+        if start <= end:
+            return start <= now < end
+        else: # over midnight e.g., 23:30-04:15
+            return start <= now or now < end
+    
+    def afterhourscheck(new_sla, message):
+        if in_between(new_sla.time(), time(17), time(23,59)):
+            alternate = datetime.strftime(datetime.combine(new_sla.date(), time(11)) + timedelta(days=1), '%d-%m-%Y %H:%M:%S')
+            message = f" - New SLA is Afterhours! - Use {alternate} unless AH is organised with customer"
+            return new_sla, message
+        elif in_between(new_sla.time(), time(0), time(7)):
+            alternate = datetime.strftime(datetime.combine(new_sla.date(), time(11)), '%d-%m-%Y %H:%M:%S')
+            message = f" - New SLA is Afterhours! - Use {alternate} unless AH is organised with customer"
+            return new_sla, message
+        elif in_between(new_sla.time(), time(7), time(10)):
+            if datetime.combine(now_date, end_delta) - now > timedelta(hours=4):
+                alternate = datetime.strftime(now() + timedelta(4), '%d-%m-%Y %H:%M:%S') 
+                message = f" - SLA Falls between 7am-10am, change to {alternate}. If afterhours, follow after hours process."
+                return new_sla, message
+        else:
+            return new_sla, message
+        
+    def returnsla(new_sla, message=""):
+        weekend_date, wknd_day = weekendcheck(new_sla)
+        new_sla, message = afterhourscheck(new_sla, message)
         new_sla = new_sla.strftime('%d-%m-%Y %H:%M:%S')
+        
         if weekend_date != "nope":
             new_weekend_date = weekend_date.strftime('%d-%m-%Y %H:%M:%S')
             weekendout = f"Calculated SLA {new_sla} is a {wknd_day}. Follow afterhours process or use {new_weekend_date} {message}"
+            print("print of message in weekend if: ", message)
             return weekendout
         else:
-            sendit = f"New SLA = {new_sla} {message}"
+            print("print of message in nonweekendif", message)
+            sendit = f"New SLA = {new_sla}{message}"
             return sendit
     
     #Calculate SLA consumed on first day of ticket creation
@@ -59,28 +87,31 @@ def getsla(CASECREATEDATETIME, SLA, CUSTSTART, CUSTEND):
         day1_sla = datetime.combine(create.date(), end_time.time()) - create
     else:
         day1_sla = end_time - start_time
-    
+    print("day 1 sla: ", day1_sla)
     #The remaining SLA is calculated by subtracting the day1 SLA usage from the total SLA (this can result in a negative number with short SLAs)
     sla_remainder = timedelta(hours=int(SLA)) - day1_sla
+    print("sla_remainder: ", sla_remainder)
     
     # if sla goes into next day, add 1 day to the day counter
     # if remainder of SLA is negative and availability end time is more than 4 hours away, set new_sla as now + 4hours
     # This part of the script allows for 'NOW' dates and time for creation of new SLA... can be passed to rules.
-    ## (this step may need revisiting to allow for after hours (or a flag created)) if remainder of SLA is negative and availability end is less than 4 hours away, add 1 day and make sla 4 hours from next days start date
+    # if remainder of SLA is negative and availability end is less than 4 hours away, add 1 day and make sla 4 hours from next days start date
     
     if sla_remainder > timedelta(hours=int(0)):
         day_counter += 1
+        print("sla remainder greater than zero day counter: ", day_counter)
     elif sla_remainder <= timedelta(0) and datetime.combine(now_date, end_delta) - now > timedelta(hours=4):
         new_sla = now + timedelta(hours=4)
-        weekend_date, wknd_day = weekendcheck(new_sla)
-        returnsla(weekend_date, wknd_day, new_sla)       
+        print("If sla reminder less than zero, and datetime nowdate and customer endtime - now is greater than 4 hours, new sla= ", new_sla)
+        output = returnsla(new_sla)       
     elif sla_remainder <= timedelta(0) and datetime.combine(now_date, end_delta) - now < timedelta(hours=4):
         new_sla = datetime.combine(now_date, start_delta) + timedelta(days=1)
-        weekend_date, wknd_day = weekendcheck(new_sla)
-        returnsla(weekend_date, wknd_day, new_sla, Delay)
+        print("sla remainder <= 0 and customers end date today is less than 4 hours from now - should send delay message")
+        output = returnsla(new_sla, Delay)
         
     # function call to loop to add days as per customer availbility and remainder of sla after adding days.
     slacount, day_counter = adddays(start_time, end_time, sla_remainder, day_counter, slacount)
+    
     #the reason for setting this as 'create.date and start time' is because we want the slacount to start from the customers available time
     #after the number of days have been added. this only comes into effect if the sla gets days added.
     new_sla = datetime.combine(create.date(), start_delta) + timedelta(days=day_counter) + slacount
@@ -88,25 +119,27 @@ def getsla(CASECREATEDATETIME, SLA, CUSTSTART, CUSTEND):
     # if new sla expiry in the past make new sla 4 hours from now. Unless 4 hours from now is outside customer available times,push to customer start tomorrow
     if new_sla < now and datetime.combine(now_date, end_delta) - now > timedelta(hours=4):
         new_sla = now + timedelta(hours=4)
-        weekend_date, wknd_day = weekendcheck(new_sla)
-        returnsla(weekend_date, wknd_day, new_sla)
+        print("if new sla in the past if statement no delay")
+        output = returnsla(new_sla, )
     elif new_sla < now and datetime.combine(now_date, end_delta) - now < timedelta(hours=4):
         new_sla = datetime.combine(now_date, start_delta) + timedelta(days=1)
-        weekend_date, wknd_day = weekendcheck(new_sla)
-        returnsla(weekend_date, wknd_day, new_sla, Delay)
+        print("if sla in the past if statement plus delay")
+        output = returnsla(new_sla, Delay)
+    else:
+        output = returnsla(new_sla)
     
-    def afterhourscheck(startTime, endTime, checkTime):
-        if startTime < endTime: 
-            return nowTime >= startTime and nowTime <= endTime 
-        else: 
-            #Over midnight: 
-            return nowTime >= startTime or nowTime <= endTime 
-    
-    weekend_date, wknd_day = weekendcheck(new_sla)
-    return returnsla(weekend_date, wknd_day, new_sla)
+    return output
     
     
+    #return returnsla(new_sla)
     
+    #def afterhourscheck(startTime, endTime, checkTime):
+    #    if startTime < endTime: 
+    #        return nowTime >= startTime and nowTime <= endTime 
+    #    else: 
+    #        #Over midnight: 
+    #        return nowTime >= startTime or nowTime <= endTime 
+     
 #Rules from old matrix
 #if SLA falls = 7am-10am then make latest available the day before IF greater than 4 hours to cust COB 
 #If > 4 hours to cust COB & SLA falls AH on that day, make SLA cust COB
