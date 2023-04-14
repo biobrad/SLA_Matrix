01577889c7791d0f0cd89f2e811de129087308cf
Brad Hart <bradhartmail@gmail.com>
2023-04-14 22:37:59 +0930

    Update tkintermatrix.py

--- tkintermatrix.py
+++ tkintermatrix.py
@@ -10,10 +10,10 @@ customtkinter.set_appearance_mode("Dark")  # Modes: "System" (standard), "Dark",
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
+        
         self.geometry("500x600")
         self.title("SLA Matrix")
         self.minsize(300, 200)
-           
         self.grid_rowconfigure(0,weight=0)
         self.grid_columnconfigure(0, weight=0)
         self.mainframe = customtkinter.CTkFrame(self, width=300, height=500)
@@ -93,7 +93,12 @@ class App(customtkinter.CTk):
                     self.textbox.insert("0.0", "AN INPUT IS MISSING OR INVALID, PLEASE CHECK")
             except ValueError:
                 self.textbox.insert("0.0", "AN INPUT IS INVALID, PLEASE CHECK")
-                           
+        
+        def copy():
+        # copy selected text to clipboard
+            self.clipboard_clear()
+            self.clipboard_append(self.selection_get())
+            
         ### GUI Main ###   
         self.my_frame = customtkinter.CTkFrame(master=self.mainframe)
         self.my_frame.grid(row=1, column=0, columnspan=2, padx=20, pady=(20, 0))                                                             
@@ -103,6 +108,13 @@ class App(customtkinter.CTk):
         self.label1.grid(row=0, column=0, columnspan=2, padx=20, pady=(20, 0))
         self.entry1 = customtkinter.CTkEntry(master=self.my_frame, validate="focusout", validatecommand=self.vcmd1, placeholder_text="1-1-2023 00:00:00")
         self.entry1.grid(row=1, column=0, columnspan=2, padx=0, pady=(5,0))
+        
+        self.RightClickMenu1 = tkinter.Menu(self.entry1, tearoff=False, background='#565b5e', fg='white', font=("", 15), borderwidth=0, bd=0)
+        self.RightClickMenu1.add_command(label="Paste", command=lambda: self.entry1.insert(tkinter.END, self.clipboard_get()))
+        self.RightClickMenu1.add_command(label="Copy", command=copy)
+        self.entry1.bind("<Button-3>", lambda event: do_popup(event, frame=self.RightClickMenu1))
+        self.bind("<1>", lambda event: event.widget.focus_set())
+        
         self.label_error1 = customtkinter.CTkLabel(self.my_frame, text="", fg_color='transparent', height=10)
         self.label_error1.grid(row=2, column=0, columnspan=2, padx=1)        
         self.label2 = customtkinter.CTkLabel(master=self.my_frame, text="Customer Availability", font=customtkinter.CTkFont(size=15, weight="bold"))
@@ -113,10 +125,24 @@ class App(customtkinter.CTk):
         self.label4.grid(row=4, column=1, padx=20, pady=0)
         self.entry2 = customtkinter.CTkEntry(master=self.my_frame, validate="focusout", validatecommand=self.vcmd2, placeholder_text="HHMM")
         self.entry2.grid(row=5, column=0, padx=20)
+        
+        self.RightClickMenu2 = tkinter.Menu(self.entry2, tearoff=False, background='#565b5e', fg='white', font=("", 15), borderwidth=0, bd=0)
+        self.RightClickMenu2.add_command(label="Paste", command=lambda: self.entry2.insert(tkinter.END, self.clipboard_get()))
+        self.RightClickMenu2.add_command(label="Copy", command=copy)
+        self.entry2.bind("<Button-3>", lambda event: do_popup(event, frame=self.RightClickMenu2))
+        self.bind("<1>", lambda event: event.widget.focus_set())
+        
         self.label_error2 = customtkinter.CTkLabel(self.my_frame, text="", fg_color='transparent', height=10)
         self.label_error2.grid(row=6, column=0, padx=1)
         self.entry3 = customtkinter.CTkEntry(master=self.my_frame, validate="focusout", validatecommand=self.vcmd3, placeholder_text="HHMM")
         self.entry3.grid(row=5, column=1, padx=20)
+        
+        self.RightClickMenu3 = tkinter.Menu(self.entry3, tearoff=False, background='#565b5e', fg='white', font=("", 15), borderwidth=0, bd=0)
+        self.RightClickMenu3.add_command(label="Paste", command=lambda: self.entry3.insert(tkinter.END, self.clipboard_get()))
+        self.RightClickMenu3.add_command(label="Copy", command=copy)
+        self.entry3.bind("<Button-3>", lambda event: do_popup(event, frame=self.RightClickMenu3))
+        self.bind("<1>", lambda event: event.widget.focus_set())
+        
         self.label_error3 = customtkinter.CTkLabel(self.my_frame, text="", fg_color='transparent', height=10)
         self.label_error3.grid(row=6, column=1, padx=1)
         self.label5 = customtkinter.CTkLabel(master=self.my_frame, text="Select SLA in Hours", font=customtkinter.CTkFont(size=15, weight="bold"))
@@ -127,8 +153,14 @@ class App(customtkinter.CTk):
         self.combobox.grid(row=8, columnspan=2, padx=20, pady=10)
         self.button = customtkinter.CTkButton(master=self.my_frame, command=button_callback, text="Get Calculated SLA", fg_color=('#028900'),border_width=2, border_color=('#028900'), hover_color=('#057103'), width=200, height=60, font=customtkinter.CTkFont(size=15, weight="bold"))
         self.button.grid(row=9, column=0, columnspan=2, padx=20, pady=(0,10))
-        self.textbox = customtkinter.CTkTextbox(master=self.mainframe, width=200, height=60)
+        self.textbox = customtkinter.CTkTextbox(master=self.mainframe, width=250, height=80)
         self.textbox.grid(row=2, column=0, columnspan=2, padx=20, pady=(0, 20), sticky="ew")
+        
+        self.RightClickMenu4 = tkinter.Menu(self.textbox, tearoff=False, background='#565b5e', fg='white', borderwidth=0, bd=0)
+        self.RightClickMenu4.add_command(label="Copy", command=copy)
+        self.textbox.bind("<Button-3>", lambda event: do_popup(event, frame=self.RightClickMenu4))
+        self.bind("<1>", lambda event: event.widget.focus_set())
+        
         self.switch_var = customtkinter.StringVar(value="off")
         self.switch_1 = customtkinter.CTkSwitch(master=self.mainframe, text="dark/light", command=self.switch_event,
                                    variable=self.switch_var, onvalue="on", offvalue="off")
@@ -137,42 +169,43 @@ class App(customtkinter.CTk):
         self.label6.grid(row=3, column=1, padx=5, pady=(20,20), sticky="e")
         ### GUI end ###
         
+        
         ### SLA Calculation Start ####
 
         def getsla(CASECREATEDATETIME, SLA, CUSTSTART, CUSTEND):
             create = datetime.strptime(CASECREATEDATETIME, "%d-%m-%Y %H:%M:%S")
-            start_time = datetime.strptime(CUSTSTART, "%H%M")
-            end_time = datetime.strptime(CUSTEND, "%H%M")
-            start_delta = datetime.time(start_time)
-            end_delta = datetime.time(end_time)
+            create_time = create.time()
+            create_date = create.date()
+            start_time = datetime.strptime(CUSTSTART, "%H%M").time()
+            end_time = datetime.strptime(CUSTEND, "%H%M").time()
             now = datetime.now()   
             now_date = now.date() 
+            now_time = now.time()
+            hours_avail = (datetime.combine(now_date, end_time) - datetime.combine(now_date, start_time))
             day_counter = 0
             slacount = 0
             new_sla=""
             Delay=" - Advise customer of delayed SLA, negotiate AH Appointment if customer requires."
             output=""
 
-
-            def adddays(start, end, remainder, day_counter, slacount):
-                hoursavail = end - start
+            def adddays(hours_avail, remainder, day_counter, slacount):
                 slacount = remainder
                 while slacount > timedelta(0):
-                    if slacount - hoursavail <= timedelta(0):
+                    if slacount - hours_avail <= timedelta(0):
                         break
                     else:
-                        slacount -= hoursavail
+                        slacount -= hours_avail
                         day_counter = day_counter + 1
+                #print("slacount and day counter=", slacount, day_counter)
                 return slacount, day_counter
 
             def weekendcheck(weekend_date):
                 if weekend_date.weekday() == 5:    # Saturday
                     day = "Saturday"
-                    weekend_date = datetime.combine(weekend_date.date(), start_delta) + timedelta(days=2)
+                    weekend_date = datetime.combine(weekend_date.date(), start_time) + timedelta(days=2)
                     return weekend_date, day 
                 elif weekend_date.weekday() == 6:  # Sunday
-                    day = "Sunday"
-                    weekend_date = datetime.combine(weekend_date.date(), start_delta) + timedelta(days=1)
+                    weekend_date = datetime.combine(weekend_date.date(), start_time) + timedelta(days=1)
                     return weekend_date, day
                 else:
                     return "nope", "nope"
@@ -183,93 +216,82 @@ class App(customtkinter.CTk):
                 else: # over midnight e.g., 23:30-04:15
                     return start <= now or now < end
 
-            def afterhourscheck(new_sla, message=""):
-                if in_between(new_sla.time(), time(17), time(23,59)):
-                    alternate = datetime.strftime(datetime.combine(new_sla.date(), time(11)) + timedelta(days=1), '%d-%m-%Y %H:%M:%S')
-                    message = f" - New SLA is Afterhours! - Use {alternate} unless AH is organised with customer"
-                    return new_sla, message
-                elif in_between(new_sla.time(), time(0), time(7)):
-                    alternate = datetime.strftime(datetime.combine(new_sla.date(), time(11)), '%d-%m-%Y %H:%M:%S')
-                    message = f" - New SLA is Afterhours! - Use {alternate} unless AH is organised with customer"
-                    return new_sla, message
-                elif in_between(new_sla.time(), time(7), time(10)):
-                    if datetime.combine(now_date, end_delta) - now > timedelta(hours=4):
-                        alternate = datetime.strftime(datetime.combine(new_sla.date(), end_delta) - timedelta(days=1), '%d-%m-%Y %H:%M:%S')
-                        message = f" - SLA Falls between 7am-10am, change to {alternate}. If afterhours, follow after hours process."
-                        return new_sla, message
+            def fivetoseven(new_sla):
+                message = ""
+                if new_sla.time() >= time(17) or new_sla.time() < time(7):
+                    if new_sla.time() < time(7):
+                        alternate = datetime.strftime(datetime.combine(new_sla.date(), time(11)), '%d-%m-%Y %H:%M:%S')
                     else:
-                        return new_sla, message
-                else:
-                    return new_sla, message
+                        alternate = datetime.strftime(datetime.combine(new_sla.date() + timedelta(days=1), time(11)), '%d-%m-%Y %H:%M:%S')
+                    message = f" - New SLA is Afterhours! - Use {alternate} unless AH is organised with customer"
+                return new_sla, message
 
             def returnsla(new_sla, message=""):
-                weekend_date, wknd_day = weekendcheck(new_sla)
-                new_sla, message = afterhourscheck(new_sla, message)
+                #print("new_sla weekday = ", new_sla.weekday())
+                if new_sla.weekday() >= 5:
+                    weekend_date, wknd_day = weekendcheck(new_sla)
+                if in_between(new_sla.time(), time(17), time(7)):
+                     new_sla, message = fivetoseven(new_sla)
+
                 new_sla = new_sla.strftime('%d-%m-%Y %H:%M:%S')
 
                 if weekend_date != "nope":
                     new_weekend_date = weekend_date.strftime('%d-%m-%Y %H:%M:%S')
                     weekendout = f"Calculated SLA {new_sla} is a {wknd_day}. Follow afterhours process or use {new_weekend_date} {message}"
-                    print("print of message in weekend if: ", message)
                     return weekendout
                 else:
-                    print("print of message in nonweekendif", message)
+                    #print("line 92 send it")
                     sendit = f"New SLA = {new_sla}{message}"
                     return sendit
 
             #Calculate SLA consumed on first day of ticket creation
             #If the create date and time is greater than the available time, subtract the create time from the daily available time to get how much SLA is used on the first day.
-
-            if create > datetime.combine(create.date(), start_time.time()) and create < datetime.combine(create.date(), end_time.time()):
-                day1_sla = datetime.combine(create.date(), end_time.time()) - create
-            elif create > datetime.combine(create.date(), end_time.time()) and create < datetime.combine(create.date(), start_time.time()) + timedelta(1):
+            if create > datetime.combine(create.date(), start_time) and create < datetime.combine(create.date(), end_time):
+                day1_sla = datetime.combine(create.date(), end_time) - create
+            elif create > datetime.combine(create.date(), end_time) and create < datetime.combine(create.date(), start_time) + timedelta(1):
                 day1_sla = create - create
             else:
-                day1_sla = end_time - start_time
-            print("day 1 sla: ", day1_sla)
-            #The remaining SLA is calculated by subtracting the day1 SLA usage from the total SLA (this can result in a negative number with short SLAs)
-            sla_remainder = timedelta(hours=int(SLA)) - day1_sla
-            print("sla_remainder: ", sla_remainder)
+                day1_sla = datetime.combine(create, end_time) - datetime.combine(create, start_time)
+            #print("day 1 sla: ", day1_sla)
 
-            # if sla goes into next day, add 1 day to the day counter
-            # if remainder of SLA is negative and availability end time is more than 4 hours away, set new_sla as now + 4hours
-            # This part of the script allows for 'NOW' dates and time for creation of new SLA... can be passed to rules.
-            # if remainder of SLA is negative and availability end is less than 4 hours away, add 1 day and make sla 4 hours from next days start date
-
+            sla_remainder = timedelta(hours=int(SLA)) - day1_sla #The remaining SLA is calculated by subtracting the day1 SLA usage from the total SLA (this can result in a negative number with short SLAs)
+            #print("sla_remainder: ", sla_remainder)
+                                # if sla goes into next day, add 1 day to the day counter  # if remainder of SLA is negative and availability end time is more than 4 hours away, set new_sla as now + 4hours
+                                # This part of the script allows for 'NOW' dates and time for creation of new SLA... can be passed to rules.
+                                # if remainder of SLA is negative and availability end is less than 4 hours away, add 1 day and make sla 4 hours from next days start date
             if sla_remainder > timedelta(hours=int(0)):
                 day_counter += 1
-                print("sla remainder greater than zero day counter: ", day_counter)
-            elif sla_remainder <= timedelta(0) and datetime.combine(now_date, end_delta) - now > timedelta(hours=4):
+                #print("sla remainder greater than zero day counter: ", day_counter)
+            elif sla_remainder <= timedelta(0) and datetime.combine(now_date, end_time) - now > timedelta(hours=4):
                 new_sla = now + timedelta(hours=4)
-                print("If sla reminder less than zero, and datetime nowdate and customer endtime - now is greater than 4 hours, new sla= ", new_sla)
+                #print("If sla reminder less than zero, and datetime nowdate and customer endtime - now is greater than 4 hours, new sla= ", new_sla)
                 output = returnsla(new_sla)       
-            elif sla_remainder <= timedelta(0) and datetime.combine(now_date, end_delta) - now < timedelta(hours=4):
-                new_sla = datetime.combine(now_date, start_delta) + timedelta(days=1)
-                print("sla remainder <= 0 and customers end date today is less than 4 hours from now - should send delay message")
-                output = returnsla(new_sla, Delay)
+            elif sla_remainder <= timedelta(0) and datetime.combine(now_date, end_time) - now < timedelta(hours=4):
+                new_sla = datetime.combine(now_date, start_time) + timedelta(days=1)
+                #print("sla remainder <= 0 and customers end date today is less than 4 hours from now - should send delay message")
+                output = returnsla(new_sla, Delay)          
 
-            # function call to loop to add days as per customer availbility and remainder of sla after adding days.
-            slacount, day_counter = adddays(start_time, end_time, sla_remainder, day_counter, slacount)
+            slacount, day_counter = adddays(hours_avail, sla_remainder, day_counter, slacount) # function call to loop to add days as per customer availbility and remainder of sla after adding days
+            new_sla = datetime.combine(create.date(), start_time) + timedelta(days=day_counter) + slacount #the reason for setting this as 'create.date and start time' is because we want the slacount to start from the customers available time #after the number of days have been added. this only comes into effect if the sla gets days added.
 
-            #the reason for setting this as 'create.date and start time' is because we want the slacount to start from the customers available time
-            #after the number of days have been added. this only comes into effect if the sla gets days added.
-            new_sla = datetime.combine(create.date(), start_delta) + timedelta(days=day_counter) + slacount
-
-            # if new sla expiry in the past make new sla 4 hours from now. Unless 4 hours from now is outside customer available times,push to customer start tomorrow
-            if new_sla < now and datetime.combine(now_date, end_delta) - now > timedelta(hours=4):
+            if new_sla < now and datetime.combine(now_date, end_time) - now > timedelta(hours=4): # if new sla expiry in the past make new sla 4 hours from now. Unless 4 hours from now is outside customer available times,push to customer start tomorrow
                 new_sla = now + timedelta(hours=4)
-                print("if new sla in the past if statement no delay")
-                output = returnsla(new_sla, )
-            elif new_sla < now and datetime.combine(now_date, end_delta) - now < timedelta(hours=4):
-                new_sla = datetime.combine(now_date, start_delta) + timedelta(days=1)
-                print("if sla in the past if statement plus delay")
+                #print("if new sla in the past if statement no delay")
+                output = returnsla(new_sla)
+            elif new_sla < now and datetime.combine(now_date, end_time) - now < timedelta(hours=4):
+                new_sla = datetime.combine(now_date, start_time) + timedelta(days=1)
+                #print("if sla in the past if statement plus delay")
                 output = returnsla(new_sla, Delay)
             else:
+                #print("line 134 returnsla - new sla= ", new_sla)
                 output = returnsla(new_sla)
 
             return output
-
-
+    
+        def do_popup(event, frame):
+            try: frame.tk_popup(event.x_root, event.y_root)
+            finally: frame.grab_release()
+    
     def switch_event(self):
         if self.switch_var == "on":
             customtkinter.set_appearance_mode("Dark")
@@ -277,7 +299,7 @@ class App(customtkinter.CTk):
         else:
             customtkinter.set_appearance_mode("Light")
             self.switch_var = "on"
-                
+    
 if __name__ == "__main__":
     app = App()
     app.mainloop()
