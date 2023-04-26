e089a020c2401b9be512a2ed8edaed152466b532
Brad Hart <bradhartmail@gmail.com>
2023-04-26 21:38:51 +0930

    Delete pcconfig.py

--- pcconfig.py
+++
@@ -1,8 +0,0 @@
-import pynecone as pc
-
-config = pc.Config(
-    app_name="newmatrix",
-    api_url="192.168.10.100:8000",
-    bun_path="$HOME/.bun/bin/bun",
-    db_url="sqlite:///pynecone.db",
-)
